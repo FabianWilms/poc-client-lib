9c07b96c66b0d9fba2744013968dc297cb170973
fabian.wilms <fabian.wilms@muenchen.de>
2017-09-27 07:19:29 +0200

    Initial commit

---
+++ src/main/java/com/example/pocclientlib/PocClientLibApplication.java
@@ -0,0 +1,12 @@
+package com.example.pocclientlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocClientLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocClientLibApplication.class, args);
+	}
+}
